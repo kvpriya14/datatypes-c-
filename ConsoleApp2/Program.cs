bc78ba355cc0cee283eb9a43772ad2cf74de0f05
HP <HP@DESKTOP-5FDOP27>
2024-09-11 10:53:55 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+internal class program
+{
+    static void Main(string[] args)
+    {
+        string X = default;
+        int a = default;
+        float b = default;
+        double c = default;
+        byte d = default;
+        sbyte e = default;
+        uint f = default;
+        short g = default;
+        ushort h = dnamespace priya;
+         {
+            default;
+            long i = default;
+            ulong j = default;
+            char k = default;
+            bool l = default;
+            object m = default;
+            decimal n = default;
+            /* console.write line(x);*/
+            Console.WriteLine("string :" + X);
+            Console.WriteLine("int    :" + a);
+            Console.WriteLine("float  :" + b);
+            Console.WriteLine("double :" + c);
+            Console.WriteLine("byte   :" + d);
+            Console.WriteLine("sbyte  :" + e);
+            Console.WriteLine("uint   :" + f);
+            Console.WriteLine("short  :" + g);
+            Console.WriteLine("ushort :" + h);
+            Console.WriteLine("long   :" + i);
+            Console.WriteLine("ulong  :" + j);
+            Console.WriteLine("char   :" + k);
+            Console.WriteLine("bool   :" + l);
+            Console.WriteLine("object :" + m);
+            Console.WriteLine("decimal:" + n);
+
+
+
+         }
+    }
+}
+
+
+
+        
+    
+
